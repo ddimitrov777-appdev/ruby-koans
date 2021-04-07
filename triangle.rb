f802203a94ce88b2dd07e45762290af5e1a714a2
ddimitrov777 <ddimitro@chicagobooth.edu>
2021-04-07 21:26:44 +0000

    Completed koan7

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,19 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+if [a,b,c].include? 0
+  raise TriangleError, "can't have zero"
+end
+
+if [a, b, c].any? { |n| n < 0 } 
+  raise TriangleError, "can't have negative sides"
+end
+
+if a + b <= c || b + c <= a || a + c <= b
+  raise TriangleError, "sum of any two sides must be greater than third"
+end
+
+
   if a == b && b == c
     return :equilateral
   end
